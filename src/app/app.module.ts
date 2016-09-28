5c31327e92abefe6bcb7046aedeb78bfa084b835
Hoang <hoangnguyen@bravesoft.vn>
2016-09-28 13:08:29 +0700

    Update ng2 material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-// Material 2 
-import { MdCoreModule } from '@angular2-material/core'
-import { MdButtonModule } from '@angular2-material/button';
-import { MdCardModule } from '@angular2-material/card';
-import { MdRadioModule } from '@angular2-material/radio';
-import { MdCheckboxModule } from '@angular2-material/checkbox'
-import { MdTooltipModule } from '@angular2-material/tooltip';
-import { MdSliderModule } from '@angular2-material/slider';
-import { MdSidenavModule } from '@angular2-material/sidenav';
-import { MdIconModule, MdIconRegistry } from '@angular2-material/icon';
-import 'hammerjs';
+import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 
@@ -25,27 +15,9 @@ import { AppComponent } from './app.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    MdCoreModule, MdCardModule, MdButtonModule, MdRadioModule,
-    MdCheckboxModule, MdTooltipModule, MdSliderModule, MdIconModule,
-    MdSidenavModule,
-
-    // MdCoreModule.forRoot(),
-    // MdCardModule.forRoot(),
-
-    // MdButtonToggleModule.forRoot(),
-    // MdDialogModule.forRoot(),
-    MdIconModule.forRoot(),
-    // MdMenuModule.forRoot(),
-    MdRadioModule.forRoot(),
-    MdTooltipModule.forRoot(),
-    MdSidenavModule.forRoot()
-    // OverlayModule.forRoot(),
+    MaterialModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-  constructor(mdIconRegistry: MdIconRegistry) {
-    mdIconRegistry.registerFontClassAlias('fontawesome', 'fa');
-  }
-}
+export class AppModule { }
