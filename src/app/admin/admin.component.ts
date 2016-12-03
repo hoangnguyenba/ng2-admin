e0ed5666365b8ebfec4d68212c948d3e1eec181e
Hoang Nguyen <hoang.nguyenba@hotmail.com>
2016-12-03 16:22:15 +0700

    Clean

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -6,9 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent {
-
-  title = 'app works!';
-
   
   menus: any[] = [
     {
@@ -20,20 +17,5 @@ export class AdminComponent {
       name: "About"
     }
   ];
-
-  foods: any[] = [
-    {name: 'Pizza', rating: 'Excellent'},
-    {name: 'Burritos', rating: 'Great'},
-    {name: 'French fries', rating: 'Pretty good'},
-  ];
-
-  progress: number = 0;
-
-  constructor() {
-    // Update the value for the progress-bar on an interval.
-    setInterval(() => {
-      this.progress = (this.progress + Math.floor(Math.random() * 4) + 1) % 100;
-    }, 200);
-  }
 
 }
